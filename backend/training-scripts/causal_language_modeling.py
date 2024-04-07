0effd143ef3e60a87f81b1573716b69155c30ede
Christopher Keibel <christopher.keibel.90@gmail.com>
2024-04-07 21:52:26 +0200

    init parse args in causal_language_modeling

---
+++ backend/training-scripts/causal_language_modeling.py
@@ -0,0 +1,15 @@
+import argparse
+from rag.utils import TrainingConfig
+
+
+def parse_args() -> TrainingConfig:
+    parser: argparse.Namespace = argparse.ArgumentParser(description="Causal Language Modeling Training Script")
+    # TODO: Add argument for config path
+    pass
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
