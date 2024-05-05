0e35bdaeec6be78bc89a6cba69f54aca27534040
Christopher Keibel <christopher.keibel.90@gmail.com>
2024-05-05 16:16:42 +0200

    restructure api

--- backend/src/multimodal_rag/main.py
+++ backend/src/multimodal_rag/main.py
@@ -1,23 +1,7 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import os
 
 
-app = FastAPI()
-
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.get("/")
-def read_root():
-    print("accesed")
-    return {"message": "Hello World!"}
+if __name__ == "__main__":
+    print(os.curdir)
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
