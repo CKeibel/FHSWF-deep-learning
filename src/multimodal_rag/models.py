ba2a3a13ab397ee66477d13265d5b43c5427f773
Christopher Keibel <christopher.keibel.90@gmail.com>
2024-05-20 11:46:38 +0000

    fix device

--- src/multimodal_rag/models.py
+++ src/multimodal_rag/models.py
@@ -10,7 +10,7 @@ class CausalLM:
         self.config = config
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.tokenizer = AutoTokenizer.from_pretrained(config.path)
-        self.model = AutoModelForCausalLM.from_pretrained(config.path)
+        self.model = AutoModelForCausalLM.from_pretrained(config.path, device_map=self.device)
 
     def __tokenize(self, text: str) -> torch.Tensor:
         """Function to tokenize the input text and return the input_ids tensor.
