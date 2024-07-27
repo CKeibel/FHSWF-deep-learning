59f2e07636c15d4d8884b5ffa9553e381adf7338
Christopher Keibel <christopher.keibel.90@gmail.com>
2024-07-27 13:04:47 +0200

    vector store placeholder

--- src/multimodal_rag/storage/vector_store.py
+++ src/multimodal_rag/storage/vector_store.py
@@ -6,15 +6,27 @@ class VectorStoreBase(ABC):
     @abstractmethod
     def query(self, query_vector: np.ndarray, k=1):  # TODO: define return type
         pass
-    
+
     @abstractmethod
-    def insert(self, documents) -> None: # TODO: Document type
+    def insert(self, documents) -> None:  # TODO: Document type
         pass
 
     @abstractmethod
-    def delete(self, document_ids) -> None: # TODO: param
+    def delete(self, document_ids) -> None:  # TODO: param
         pass
 
 
 class LanceDB(VectorStoreBase):
+    pass
+
+
+class ChromaDB(VectorStoreBase):
+    pass
+
+
+class PGVector(VectorStoreBase):
+    pass
+
+
+class Qdrant(VectorStoreBase):
     pass
