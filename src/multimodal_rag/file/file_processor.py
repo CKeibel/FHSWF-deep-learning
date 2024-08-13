43cf8aa455c1bc35a601948b981606b952ad0b6e
Christopher Keibel <christopher.keibel.90@gmail.com>
2024-08-13 07:06:46 +0200

    init file upload functionality

--- src/multimodal_rag/file/file_processor.py
+++ src/multimodal_rag/file/file_processor.py
@@ -1,6 +1,9 @@
 # Strategy pattern
 from abc import ABC, abstractmethod
 from pathlib import Path
+from gradio.utils import NamedString
+from loguru import logger
+from multimodal_rag.enums.file_extentions import FileExtensions
 
 
 class FileProcessor(ABC):
@@ -17,17 +20,34 @@ class FileProcessor(ABC):
 
 class UnstructuredIOFileProcessor(FileProcessor):
     @staticmethod
-    def process(files) -> None:  # TODO
-        file_path = [file.name for file in files]
+    def process(files: list[NamedString]) -> None:  # TODO
+        for file in files:
+            try:
+                file_path = Path(file)
+            except Exception as e:
+                logger.error(f"Error file not readable documents: {e}")
+                continue
+            
+            # Standardize documents
+            UnstructuredIOFileProcessor.__standardize_documents(file_path)
+
+            # Process documents
+            UnstructuredIOFileProcessor.process(file)
 
     @staticmethod
     def chunk() -> None:  # TODO
         pass
 
     @staticmethod
-    def __standardize_documents(file) -> None:
+    def __standardize_documents(file_path: Path) -> None:
+        with open(file_path, "rb") as f:
+            pass
+
         # Get file extension (MIME type)
-        pass
+        file_extension = file_path.suffix
+
+        if file_extension == FileExtensions.PDF:
+            pass        
 
 
 class PyPDFFileProcessor(FileProcessor):
